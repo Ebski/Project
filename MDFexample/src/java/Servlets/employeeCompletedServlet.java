824ff611dee38a74b70595179f22ea53f50764f7
Ebski <ebbe_vig@hotmail.com>
2015-04-20 12:12:07 +0200

    3 new servlets for employee views

---
+++ MDFexample/src/java/Servlets/employeeCompletedServlet.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import DTO.CampaignDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import logic.FetchRequest;
+
+/**
+ *
+ * @author Ebbe
+ */
+@WebServlet(name = "employeeCompletedServlet", urlPatterns = {"/employeeCompletedServlet"})
+public class employeeCompletedServlet extends HttpServlet {
+
+    FetchRequest FR = new FetchRequest();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ArrayList<CampaignDTO> result = null;
+
+        try {
+            result = FR.fetchPendingCampaigns();
+        } catch (SQLException ex) {
+            Logger.getLogger(mdfServlets.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute("Campaigns", result);
+
+        RequestDispatcher disp = request.getRequestDispatcher("employeeViewCompleted.jsp");
+        disp.forward(request, response);
+
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
